f72fb965fdefebe385dd1333e79baa887705799d
Wesly Laboy <wesly.laboy@upr.edu>
2025-12-25 16:27:18 -0400

    Bar and Pie charts

--- 2_2_bar_and_pie_charts_matplotlib/1_theory/task/data.py
+++ 2_2_bar_and_pie_charts_matplotlib/1_theory/task/data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from common.paths import GAMES_DATASET_PATH
@@ -31,9 +32,52 @@ def preprocess(data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
+def __add_decades(data: pd.DataFrame) -> pd.DataFrame:
+    data = data.copy()
+
+    decade_bins = (
+        np.array(range(data["year_of_release"].min() // 10 * 10, data["year_of_release"].max() // 10 * 10 + 11, 10)) - 1
+    )
+
+    # Dropping the last decade because of incomplete sales data
+    decade_bins = decade_bins[:-1]
+    decade_labels = np.array(decade_bins[:-1]) + 1
+
+    data["decade"] = pd.cut(data["year_of_release"], bins=decade_bins, labels=decade_labels, right=True)
+    return data[data["decade"].notna()]
+
+
+def __extract_sales_region(data: pd.DataFrame) -> pd.DataFrame:
+    sales_columns = {"eu_sales", "jp_sales", "na_sales", "other_sales"}
+    other_columns = set(data.columns.unique()).difference(sales_columns)
+
+    data = data.melt(id_vars=other_columns, value_vars=sales_columns, value_name="sales", var_name="region")
+    data["region"] = data["region"].str.replace("_sales", "")
+
+    return data
+
+
 def aggregate(games: pd.DataFrame) -> pd.DataFrame:
-    return games["platform"].value_counts().reset_index()
+    games = __add_decades(games)
+    games = __extract_sales_region(games)
+    return games.groupby(["decade", "region"], observed=True)["sales"].sum().reset_index()
 
 
-def filter_platforms(data: pd.DataFrame) -> pd.DataFrame:
-    return data[data["platform"].isin(["PS4", "XOne", "PC", "WiiU"])]
+def get_number_of_decades(data: pd.DataFrame) -> int:
+    return data["decade"].nunique()
+
+
+def get_number_of_regions(data: pd.DataFrame) -> int:
+    return data["region"].nunique()
+
+
+def get_region_sales(data: pd.DataFrame, region: str) -> pd.Series:
+    return data[data["region"] == region]["sales"]
+
+
+def get_all_regions(data: pd.DataFrame) -> list[str]:
+    return data.groupby("region")["sales"].sum().sort_values(ascending=False).index.tolist()
+
+
+def get_all_decades(data: pd.DataFrame) -> list[int]:
+    return list(data["decade"].unique())
--- 2_2_bar_and_pie_charts_matplotlib/1_theory/task/task.py
+++ 2_2_bar_and_pie_charts_matplotlib/1_theory/task/task.py
@@ -1,24 +1,47 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from data import preprocess, read, aggregate, filter_platforms
+from data import aggregate, get_number_of_decades, get_region_sales, preprocess, read, get_all_regions, get_number_of_regions, get_all_decades
 
 
 def plot_region(ax: plt.Axes, games: pd.DataFrame, region: str, trace: int = 0):
-    # TODO: do not implement this function until the corresponding task
-    pass
+    number_of_groups = get_number_of_decades(games)
+    region_sales = get_region_sales(games, region)
+    number_of_regions = get_number_of_regions(games)
+
+    width = 1
+    positions = []
+    # print(decades)
+    for j in range(number_of_groups):
+        positions.append(j+ j*number_of_regions + width * trace)
+
+    ax.bar(positions, region_sales, width, label=region)
+    ax.legend()
 
 
 def plot(games: pd.DataFrame) -> plt.Figure:
-    df = filter_platforms(aggregate(games))
-    print(df.shape)
+    games = aggregate(games)
+    regions = get_all_regions(games)
+    number_of_groups = get_number_of_decades(games)
+    decades = get_all_decades(games)
+    number_of_regions = get_number_of_regions(games)
+
 
     fig, ax = plt.subplots()
-    ax.pie(data = df, labels= 'platform', x= 'count',)
+    for i, region in enumerate(regions):
+        plot_region(ax, games, region, trace=i)
 
+    width = 1
+
+    ticks = [j + j * number_of_regions + (number_of_regions - 1) * width / 2 for j in range(number_of_groups)]
+    ax.set_xticks(ticks, labels=[str(decade) for decade in decades])
+
+    ax.set_xlabel("Decade")
+    ax.set_ylabel('Sales')
+    ax.set_title('Total sales for each region over decades')
     fig.tight_layout()
-    # plt.show()
     return fig
+
 
 # Please solve the task in the plot function and do not modify this one
 def main():
